5258cc88059a2b66751158dc9625135928fb86f9
Sean Hsieh <hsiehs@fortinet.com>
2023-09-07 09:11:19 +0800

    Add the schema.json

--- my-component/src/my-component/index.ts
+++ my-component/src/my-component/index.ts
@@ -1,10 +1,13 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Schema } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function myComponent(_options: any): Rule {
+export function myComponent(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    const { name } = _options; // 從 _options 讀取 name
+
+    tree.create('hello.js', `console.log('Hello ${name}');`);
     return tree;
   };
 }
